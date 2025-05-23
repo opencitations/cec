fad94957f4ee8ef9538706eb5f1503f062d0de4a
Ivan Heibi <ivanhb.ita@gmail.com>
2025-05-23 16:19:22 +0200

    update prefix in settings

--- extractor/cex/settings.py
+++ extractor/cex/settings.py
@@ -3,10 +3,12 @@ from pathlib import Path
 # Define the root directory of the project
 ROOT_DIR = Path(__file__).resolve().parent.parent  # Adjust based on your project structure
 
+ROOT_DIR = "/cex/"
+
 # Define the path to the predefined mappings file
-PREDEFINED_MAPPINGS_PATH = '/semantic_alignment/predefined_mappings.json'
-SPECIAL_CASES_PATH = '/special_cases.json'
-CONFIG_PATH = '/config.json'
-UPLOAD_FOLDER = '/static/files'
-DOWNLOAD_FOLDER = '/static/output'
-PROCESSING_FOLDER = '/static/processing'
+PREDEFINED_MAPPINGS_PATH = ROOT_DIR+'semantic_alignment/predefined_mappings.json'
+SPECIAL_CASES_PATH = ROOT_DIR+'special_cases.json'
+CONFIG_PATH = ROOT_DIR+'config.json'
+UPLOAD_FOLDER = ROOT_DIR+'static/files'
+DOWNLOAD_FOLDER = ROOT_DIR+'static/output'
+PROCESSING_FOLDER = ROOT_DIR+'static/processing'
