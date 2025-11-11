1f00d732e2d24c99df51364bdd3e46179e2cb853
rempairamore <rempairamore@gmail.com>
2025-11-11 15:33:22 +0100

    fixing bug in main.py

--- extractor/cex/main.py
+++ extractor/cex/main.py
@@ -280,6 +280,8 @@ def create_app():
 
     return app
 
+# Create the Flask app that works with Gunicorn
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     app.run(host='0.0.0.0', port=5001, debug=False)
