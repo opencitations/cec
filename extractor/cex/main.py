44f61649805b0e55549bbc8f2c4965a066b91f90
rempairamore <rempairamore@gmail.com>
2025-11-12 14:39:42 +0100

    fixing path and docker verison

--- extractor/cex/main.py
+++ extractor/cex/main.py
@@ -139,9 +139,9 @@ def create_app():
 
         return send_from_directory(download_location, filename, as_attachment=True)
     
-    @app.route(PREFIX+'/sample/<filename>', methods=['GET'])
+    @app.route(PREFIX+'/downloads/sample/<filename>', methods=['GET'])
     def download_sample(filename):
-        sample_location = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'downloads', 'sample')
+        sample_location = os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['DOWNLOAD_FOLDER'], 'sample')
         return send_from_directory(sample_location, filename, as_attachment=True)
 
     from api.routes import api_blueprint
